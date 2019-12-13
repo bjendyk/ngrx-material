01e75bbcb6a5004bfb10e54020e3fadaca9ad530
bjendyk <bartlomiej.jendyk@onwelo.com>
2019-12-13 11:46:48 +0100

    UT for NavbarComponent

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,40 +1,46 @@
-import { LayoutModule } from '@angular/cdk/layout';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { of } from 'rxjs';
 
 import { NavbarComponent } from './navbar.component';
+import { LibraryImportsModule } from '../../library-imports.module';
+
+const breakpointObserverStub = jasmine.createSpyObj('BreakpointObserver', ['observe']);
+breakpointObserverStub.observe.and.returnValue({
+  pipe: () => of(true)
+});
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+  let component;
+  let fixture;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NavbarComponent],
+      declarations: [ NavbarComponent ],
       imports: [
         NoopAnimationsModule,
-        LayoutModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
+        LibraryImportsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: BreakpointObserver, useValue: breakpointObserverStub }
       ]
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(NavbarComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('isHandset$ should resolve to true', () => {
+    component.isHandset$.subscribe((result) => {
+      expect(result).toBe(true);
+    });
   });
 });
